113b5a0a2129612884660d838d98c536c995def5
Marta Pretel <marpreal97@gmail.com>
2025-03-06 12:24:56 +0100

    feat: add deploy script for Vercel

--- src/api/marvelApi.ts
+++ src/api/marvelApi.ts
@@ -4,11 +4,13 @@ import md5 from "md5";
 const publicKey = import.meta.env.VITE_MARVEL_PUBLIC_KEY;
 const privateKey = import.meta.env.VITE_MARVEL_PRIVATE_KEY;
 const baseApiUrl = import.meta.env.VITE_API_BASE_URL;
+const secureBaseUrl = baseApiUrl.replace(/^http:/, "https:");
+
 const ts = new Date().getTime();
 const hash = md5(`${ts}${privateKey}${publicKey}`);
 
 const api = axios.create({
-  baseURL: baseApiUrl,
+  baseURL: secureBaseUrl,
   headers: {
     "Content-Type": "application/json",
   },
